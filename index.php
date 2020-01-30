93ac50e7eef7adb9d7ad3264077347edc1113054
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-01-30 04:38:30 -0800

    added a calc... not done

--- index.php
+++ index.php
@@ -5,8 +5,22 @@
  </head>
  <body>
 	<h1>POS System</h1>
+	**display input(prices)***
 	<div>
-		<p>Testing out page...Crossing my fingers!</p>
+		<input type="button" value="1">
+		<input type="button" value="2">
+		<input type="button" value="3">
+		<input type="button" value="+">
+		<br>
+		<input type="button" value="4">
+		<input type="button" value="5">
+		<input type="button" value="6">
+		<input type="button" value="-">
+		<br>
+		<input type="button" value="7">
+		<input type="button" value="8">
+		<input type="button" value="9">
+		<input type="button" value="*">
 	</div>
 	<script src="./app.js"></script>
  </body>
