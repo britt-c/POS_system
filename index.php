b885f88b03a726785f5bd9047f20da07da5c3cc7
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-01-30 05:37:29 -0800

    playing with forms and inputs

--- index.php
+++ index.php
@@ -5,23 +5,30 @@
  </head>
  <body>
 	<h1>POS System</h1>
-	**display input(prices)***
+	<form name="pos">
+	**display input(prices) here***
+		<input type="" name="price">
 	<div>
 		<input type="button" value="1">
 		<input type="button" value="2">
 		<input type="button" value="3">
-		<input type="button" value="+">
+		<input type="button" value="Add Amount">
 		<br>
 		<input type="button" value="4">
 		<input type="button" value="5">
 		<input type="button" value="6">
-		<input type="button" value="-">
+		<input type="button" value="Save Till">
 		<br>
 		<input type="button" value="7">
 		<input type="button" value="8">
 		<input type="button" value="9">
-		<input type="button" value="*">
+		<br>
+		<input type="button" value="C">
+		<input type="button" value="0">
+		<input type="button" value=".">
+		<br>
 	</div>
+	</form>
 	<script src="./app.js"></script>
  </body>
 </html>
