5d74a45af079f48d8e889de1507926ef4da1e583
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-01-30 03:14:42 -0800

    linked css and js file to php file. testing out the pages to make sure everything connects properly.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <head>
+<link rel="stylesheet" type="text/css" href="app.css">
+ </head>
+ <body>
+	<h1>POS System</h1>
+	<div>
+		<p>Testing out page...Crossing my fingers!</p>
+	</div>
+	<script src="./app.js"></script>
+ </body>
+</html>
+
