8538feaa3b4592c10d20cb6d825f89ecda5d8310
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-02-03 07:58:22 -0800

    fixed screen so you dont have to scroll down when entering in numbers... also linked the submit pg to the js and css files

--- submit.php
+++ submit.php
@@ -1,9 +1,11 @@
 <html>
 	<head>
+	<link rel="stylesheet" type="text/css" href="app.css">
 		<h1>Till Page</h1>
 	</head>
 	<body>
 		Entry 1: <?php echo $_POST["saveledg"]; ?> 
+	<script src="./app.js"></script> 
 	</body>
 </html>
 
