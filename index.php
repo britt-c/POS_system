2109b042c42a0b0f16021f2006e5e601369ebcf5
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-02-02 16:46:09 -0800

    numbers now show up in the bar above the buttons.

--- index.php
+++ index.php
@@ -11,20 +11,20 @@
 		<br>
 		<br>
 	<div class="buttons">
-		<input class="btn" type="button" value="1">
-		<input class="btn" type="button" value="2">
-		<input class="btn" type="button" value="3">
+		<input class="btn" type="button" value="1" onclick="pos.price.value += '1'">
+		<input class="btn" type="button" value="2" onclick="pos.price.value += '2'">
+		<input class="btn" type="button" value="3" onclick="pos.price.value += '3'">
 		<br>
-		<input class="btn" type="button" value="4">
-		<input class="btn" type="button" value="5">
-		<input class="btn" type="button" value="6">
+		<input class="btn" type="button" value="4" onclick="pos.price.value += '4'">
+		<input class="btn" type="button" value="5" onclick="pos.price.value += '5'">
+		<input class="btn" type="button" value="6" onclick="pos.price.value += '6'">
 		<br>
-		<input class="btn" type="button" value="7">
-		<input class="btn" type="button" value="8">
-		<input class="btn" type="button" value="9">
+		<input class="btn" type="button" value="7" onclick="pos.price.value += '7'">
+		<input class="btn" type="button" value="8" onclick="pos.price.value += '8'">
+		<input class="btn" type="button" value="9" onclick="pos.price.value += '9'">
 		<br>
 		<button type="button" class="btn" disabled>💰</button>
-		<input class="btn" type="button" value="0">
+		<input class="btn" type="button" value="0" onclick="pos.price.value += '0'">
 		<button type="button" class="btn" disabled>💰</button>
 		<br>
 		<input class="btn" id="add" type="button" value="+ Ledger">
